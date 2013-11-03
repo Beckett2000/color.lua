2894cf8dfff03860823e60007a6b3b896d8d377c
Beckett2000 <Beckett2000@gmail.com>
2013-11-03 11:39:43 -0500

    Updated Function
    Non Standard LCH Color space has been replaced with more useful YCbCr.

--- Conversion.lua
+++ Conversion.lua
@@ -1,10 +1,18 @@
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- Color Space Conversion Functions --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
 -- Converts Between Color Spaces and Conversions
--- Accepts/Outputs RGB/HSV/HSL/HSI/LCH 601/LCH 709/Hex
+-- Accepts/Outputs RGB/HSV/HSL/HSI/YCbCr 601/YCbCr 709/Hex
 function ColorSpace(input,output,...)
     
     local In if input and type(input) == "string" then In = string.upper(input) 
+      if In == "YCC 601" or In == "YCBCR 601" then In = "YCbCr 601" end
+      if In == "YCC 709" or In == "YCBCR 709" then In = "YCbCr 709" end
      else print("Input must be valid string.") return end
     local Out if output and type(output) == "string" then Out = string.upper(output)
+      if Out == "YCC 601" or Out == "YCBCR 601" then Out = "YCbCr 601" end
+      if Out == "YCC 709" or Out == "YCBCR 709" then Out = "YCbCr 709" end
      else print("Output must be valid string.") return end
     
     local Values = {...}
@@ -16,21 +24,17 @@ function ColorSpace(input,output,...)
     local function ConversionNS(Input,values) 
         
      -- Simple Hue Chroma Conversion Model
-     if Input == "HSV" or Input == "HSL" or Input == "LCH 601" or Input == "LCH 709" then
+     if Input == "HSV" or Input == "HSL" then
+        
       local Values = {} -- Color Table Evaluation
-    
       if Input == "HSV" then Values.h = values.h or values[1] Values.s = values.s or values[2]
        Values.v = values.v or values[3] Values.s = Values.s / 100 Values.v = Values.v / 100
       elseif Input == "HSL" then Values.h = values.h or values[1] Values.s = values.s or values[2] 
-       Values.l = values.l or values[3] Values.s = Values.s / 100 Values.l = Values.l / 100
-      elseif Input == "LCH 601" or In == "LCH 709" then Values.y = values.y or values[1]
-       Values.c = values.c or values[2] Values.h = values.h or values[3] 
-       Values.y = Values.y / 100 Values.c = Values.c / 100  end
+       Values.l = values.l or values[3] Values.s = Values.s / 100 Values.l = Values.l / 100 end
     
       local Hdash,RGB = Values.h/60,{R = 0, G = 0, B = 0}
       local Chroma if Input == "HSV" then Chroma = Values.v * Values.s
-      elseif Input == "HSL" then Chroma = (1 - math.abs((2 * Values.l) - 1)) * Values.s
-      elseif Input == "LCH 601" or Input == "LCH 709" then Chroma = Values.c end
+      elseif Input == "HSL" then Chroma = (1 - math.abs((2 * Values.l) - 1)) * Values.s end
       local X = Chroma * (1.0 - math.abs((Hdash % 2.0) - 1.0))
      
       if Hdash < 1.0 then RGB.R = Chroma  RGB.G = X
@@ -42,9 +46,7 @@ function ColorSpace(input,output,...)
     
       local R,G,B = RGB.R,RGB.G,RGB.B
       local Min if In == "HSV" then Min = Values.v - Chroma
-      elseif Input == "HSL" then Min = Values.l - (0.5 * Chroma)
-      elseif Input == "LCH 601" then Min = Values.y - ((0.2990 * R) + (0.5864 * G) + (0.1146 * B))
-      elseif Input == "LCH 709" then Min = Values.y - ((0.2126 * R) + (0.7152 * G) + (0.0722 * B)) end
+      elseif Input == "HSL" then Min = Values.l - (0.5 * Chroma) end
       RGB.R = RGB.R + Min RGB.G = RGB.G + Min RGB.B = RGB.B + Min
       return {r = RGB.R * 255, g = RGB.G * 255, b = RGB.B * 255} 
     
@@ -57,10 +59,20 @@ function ColorSpace(input,output,...)
        R =  i * (1+((s * math.cos(h))/math.cos((math.pi/3) - h))) G = (3 * i)-(B + R) 
       elseif h >= (2*math.pi)/3 and h < (4 * math.pi)/3 then h = h-((2*math.pi)/3) R = i*(1-s) 
        G = i * (1+((s * math.cos(h))/math.cos((math.pi/3) - h))) B = (3 * i)-(R + G) 
-      elseif h >= (4 * math.pi)/3 and h < 2 * math.pi then h = h-((4*math.pi)/3) G = i*(1-s) 
+      elseif h >= (4 * math.pi)/3 and h <= 2 * math.pi then h = h-((4*math.pi)/3) G = i*(1-s) 
        B =  i * (1+((s * math.cos(h))/math.cos((math.pi/3) - h))) R = (3 * i)-(G + B) end
-    
       return {r = R * 255, g = G * 255, b = B * 255} 
+    
+     elseif Input == "YCbCr 601" or Input == "YCbCr 709" then -- Converts YCbCr to RGB
+      local Vals = {} Vals.Y = values.Y or values[1] Vals.Cb = values.Cb or values[2]
+       Vals.Cr = values.Cr or values[3] 
+    
+      local RGB,Luma,Cb,Cr = {},Vals.Y/100,Vals.Cb/100,Vals.Cr/100
+      local Kr,KB if Input == "YCbCr 601" then Kr = 0.2990 Kb = 0.1146
+       elseif Input == "YCbCr 709" then Kr = 0.2126 Kb = 0.0722 end
+      RGB.R = Cr * 2 * (1 - Kr) + Luma RGB.B = Cb * 2 * (1 - Kb) + Luma
+      RGB.G = (Luma - (RGB.R * Kr) - (RGB.B * Kb)) / (1 - Kr - Kb)
+      return {r = RGB.R * 255, g = RGB.G * 255, b = RGB.B * 255}
     
      elseif Input == "HEX" then -- Converts Hex String to RGB
       local Hexes = {A = 10, B = 11, C = 12, D = 13, E = 14, F  = 15}
@@ -84,7 +96,7 @@ function ColorSpace(input,output,...)
       local R,G,B = values.r or values[1], values.g or values[2], values.b or values[3]
     
       -- Simple Hue Chroma Conversion Model
-      if Output == "HSV" or Output == "HSL" or Output == "LCH 601" or Output == "LCH 709" then
+      if Output == "HSV" or Output == "HSL" then
        local R,G,B = R/255, G/255, B/255 -- Normalizes Color Values
     
        local Max,Min = math.max(R,G,B),math.min(R,G,B)
@@ -99,18 +111,12 @@ function ColorSpace(input,output,...)
        if Output == "HSV" then -- Hue Saturation Value Space
         local V,S = Max if Chroma == 0 then S = 0 else S = Chroma/V end
         return {h = Hue, s = S * 100, v = V * 100}
-       elseif Output == "HSL" then -- sHue Saturation Lightness Space
+       elseif Output == "HSL" then -- Hue Saturation Lightness Space
         local L,S = 0.5 * (Max + Min), 0
         if Chroma == 0 then S = 0 else S = Chroma / (1 - math.abs((2 * L) - 1)) end
-        return {h = Hue, s = S * 100, l = L * 100}
-       elseif Output == "LCH 601" then -- Luma Chroma Hue (NTSC) Space
-        local Luma = (0.2990 * R) + (0.5864 * G) + (0.1146 * B)
-        return {y = Luma * 100, c = Chroma * 100, h = Hue}
-       elseif Output == "LCH 709" then -- Luma Chroma Hue (sRGB) Space
-        local Luma = (0.2126 * R) + (0.7152 * G) + (0.0722 * B)
-        return {y = Luma * 100, c = Chroma * 100, h = Hue} end
+        return {h = Hue, s = S * 100, l = L * 100} end
         
-      elseif Output == "HSI" then -- Converts Entries to HSI Space
+      elseif Output == "HSI" then -- Hue Saturation Intensity HSI Space
        local r,g,b = R/(R+G+B),G/(R+G+B),B/(R+G+B)
        local Hue = math.acos(0.5*((r-g)+(r-b))/math.sqrt(math.pow((r-g),2)+(r-b)*(g-b)))
        local Min,Ity,Sat = math.min(r,g,b),(R + G + B)/(3 * 255) 
@@ -118,9 +124,18 @@ function ColorSpace(input,output,...)
        if b > g then Hue = (2 * math.pi) - Hue end 
        return {h = (Hue * 180)/math.pi, s = Sat * 100, i = Ity * 255}
     
+      -- Luma Chroma Blue Chroma Red (NTSC/HD)(Analog)
+      elseif Output == "YCbCr 601" or Output == "YCbCr 709" then 
+       local R,G,B,Kr,Kb = R/255, G/255, B/255
+       if Output == "YCbCr 601" then Kr = 0.2990 Kb = 0.1146
+       elseif Output == "YCbCr 709" then Kr = 0.2126 Kb = 0.0722 end
+       local Luma = (Kr * R) + ((1 - Kr - Kb) * G) + (Kb * B)
+       local Cb,Cr = 0.5 * ((B - Luma)/(1 - Kb)), 0.5 * ((R - Luma)/(1 - Kr))
+       return {Y = Luma * 100, Cb = Cb * 100, Cr = Cr * 100}
+    
       elseif Output == "HEX" then -- Converts Entry to Hex String
        local Hex,Val = "#",{"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"}
-       local RGB,Hex = {R * 255,G * 255,B * 255},"#" for _,Col in pairs(RGB) do
+       local RGB,Hex = {R * 255,G * 255,B * 255},"#" for _,Col in pairs(RGB) do 
         local Tens,Ones = math.floor(Col/16), Col % 16 Hex = Hex..Val[Tens + 1]..Val[Ones + 1] end 
        return Hex end end
     
